179c186b1382cc2ab5c0f22cd2adcc924040769f
twsho <twsho1996@gmail.com>
2025-08-19 12:45:53 +0900

    キャッシュクリア処理を一時的にコメントアウト

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -2,14 +2,14 @@ import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import './pageStyles.css';
-import { clearAllCache } from '../utils/cache';
+// import { clearAllCache } from '../utils/cache';
 
 const TopPage = () => {
-  // Clear cache when TopPage is mounted
-  useEffect(() => {
-    console.log('TopPage mounted - clearing all cache');
-    clearAllCache();
-  }, []);
+  // キャッシュクリア処理を一時的にコメントアウト
+  // useEffect(() => {
+  //   console.log('TopPage mounted - clearing all cache');
+  //   clearAllCache();
+  // }, []);
 
   return (
     <div className="top-page">
