32f2e5eb35814bb38d9b27a7ac8bd8094e83b998
twsho <twsho1996@gmail.com>
2025-08-25 00:57:21 +0900

    デプロイ設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import './components/styles.css'
 import TopPage from './pages/TopPage'
-import PayrollQueryPage from './pages/PayrollQueryPage'
+import PayrollQueryPage from './pages/PayrollQueryPage';
+import PayrollCreatePage from './pages/PayrollCreatePage';
 import PayrollDetailPage from './pages/PayrollDetailPage'
 import EmployeeManagementPage from './pages/EmployeeManagementPage'
 import EmployeeManagementDetailPage from './pages/EmployeeManagementDetailPage'
 import PayrollManagementPage from './pages/PayrollManagementPage'
 import PayrollEditPage from './pages/PayrollEditPage';
-import PayrollCreatePage from './pages/PayrollCreatePage';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<TopPage />} />
-          <Route path="/payroll-query" element={<PayrollQueryPage />} />
-          <Route path="/payroll/:employeeId/:year/:month" element={<PayrollDetailPage />} />
-          <Route path="/employee-management" element={<EmployeeManagementPage />} />
-          <Route path="/employee-management/create" element={<EmployeeManagementDetailPage />} />
-          <Route path="/employee-management/:employee_id" element={<EmployeeManagementDetailPage />} />
-          <Route path="/payroll-management" element={<PayrollManagementPage />} />
-          <Route path="/payroll-edit/:employeeId/:year/:month" element={<PayrollEditPage />} />
-          <Route path="/payroll-create/:employeeId/:year/:month" element={<PayrollCreatePage />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<TopPage />} />
+        <Route path="/payroll-query" element={<PayrollQueryPage />} />
+        <Route path="/payroll/:employeeId/:year/:month" element={<PayrollDetailPage />} />
+        <Route path="/employee-management" element={<EmployeeManagementPage />} />
+        <Route path="/employee-management/create" element={<EmployeeManagementDetailPage />} />
+        <Route path="/employee-management/:employee_id" element={<EmployeeManagementDetailPage />} />
+        <Route path="/payroll-management" element={<PayrollManagementPage />} />
+        <Route path="/payroll-edit/:employeeId/:year/:month" element={<PayrollEditPage />} />
+        <Route path="/payroll-create/:employeeId/:year/:month" element={<PayrollCreatePage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
